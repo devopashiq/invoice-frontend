7097f45a1fe3f2c856c30fc8db7d1daaeb5ee72c
ashiq <ashiq@dotserp.com>
2026-06-16 12:23:29 +0530

    completed the mvp

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://invoice-backend-73fk.onrender.com',
         changeOrigin: true,
       }
     }
